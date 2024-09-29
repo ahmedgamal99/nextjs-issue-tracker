2e7f0d5e3231674bf452a5ca9e1f1ee832e9c0c3
Ahmed Gamal <victorylover987@gmail.com>
2024-09-29 19:11:44 -0400

    setup radix UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
 import NavBar from "./NavBar";
+import { Theme } from "@radix-ui/themes";
 
 export default function RootLayout({
   children
@@ -9,8 +11,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
