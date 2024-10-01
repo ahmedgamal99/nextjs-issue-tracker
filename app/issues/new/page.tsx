658e98962fe0be962c55b4eee3d1c1598b6e2359
Ahmed Gamal <victorylover987@gmail.com>
2024-10-01 18:50:42 -0400

    refactored inline function in new issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -33,6 +33,17 @@ const NewIssuePage = () => {
   const [error, setError] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setIsSubmitting(false);
+      setError("An Unexpected Error Occured");
+    }
+  });
+
   return (
     <div className="max-w-xl ">
       {error && (
@@ -41,19 +52,7 @@ const NewIssuePage = () => {
         </Callout.Root>
       )}
 
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setIsSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setIsSubmitting(false);
-            setError("An Unexpected Error Occured");
-          }
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root placeholder="Title" {...register("title")} />
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
