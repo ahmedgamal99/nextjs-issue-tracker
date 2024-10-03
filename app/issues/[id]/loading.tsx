842115c76d2535b4da26fd6603958d98342bb5f7
Ahmed Gamal <victorylover987@gmail.com>
2024-10-03 19:02:29 -0400

    refactor code by organizing import statements

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Flex, Card, Box } from "@radix-ui/themes";
+import { Skeleton } from "@/app/components";
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box>
+      <Skeleton className="max-w-xl" />
+      <Flex className="space-x-3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,6 @@
-import React from "react";
 import { Table } from "@radix-ui/themes";
-import Link from "../components/Link";
+import { Link, IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
