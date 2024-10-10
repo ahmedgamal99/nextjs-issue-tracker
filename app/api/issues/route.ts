dc542dfcbb206725e1d9a65641caa43f8806593a
Ahmed Gamal <victorylover987@gmail.com>
2024-10-10 19:55:23 -0400

    secure application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,8 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "../../validationSchemas";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
 
@@ -10,7 +15,6 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  // Create the new issue in the database using Prisma
   const newIssue = await prisma.issue.create({
     data: {
       title: body.title,
