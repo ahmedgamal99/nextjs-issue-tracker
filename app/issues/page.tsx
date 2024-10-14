35eadd19421ecc4f40af8209afe9da085d7e5bf4
Ahmed Gamal <victorylover987@gmail.com>
2024-10-14 18:55:36 -0400

    sort issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -17,10 +17,15 @@ const IssuesPage = async ({ searchParams }: Props) => {
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" }
   ];
+
+  const orderBy = columns.map((column) => column.value).includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status
-    }
+    },
+    orderBy
   });
 
   return (
